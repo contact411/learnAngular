0e3467405fdd403febaddec065cc7c7af882d8a2
Joel Hampton <joelhampton@gmail.com>
2015-05-25 15:44:17 +1000

    Directives, Modules, Controllers, and Expressions

--- app.js
+++ app.js
@@ -2,13 +2,23 @@
   var app = angular.module('store', [ ]);
 
   app.controller('StoreController', function(){   
-    this.product = gem;
+    this.products = gems;
   });
 
-  var gem = {
-    name: 'Dodecahedron',
-    price: 2.95,
-    description: 'Blue shinny stone of the southern planet Narth.',
-    canPurchase: true
-  };
+  var gems = [ 
+    {
+      name: 'Dodecahedron',
+      price: 2.95,
+      description: 'Blue shinny stone of the southern planet Narth.',
+      canPurchase: true,
+      soldOut: false,
+    },
+    {
+      name: 'Neptune Diamond',
+      price: 42.95,
+      description: 'The clearest diamond in the galaxy.',
+      canPurchase: true,
+      soldOut: false,
+    }
+  ];
 })();
